67b7ef9c9f77ba28cd3db1cd9b16c997c7ca758f
Andrew Evans <andy@heyzap.com>
2011-06-17 12:20:49 -0700

    Download images, replace in post url

--- exporter.rb
+++ exporter.rb
@@ -1,5 +1,6 @@
 require 'httparty'
 require 'yaml'
+require 'json'
 require 'progressbar'
 
 PER_PAGE = 10 #number of posts Posterous' api returns per page
@@ -7,10 +8,22 @@ PER_PAGE = 10 #number of posts Posterous' api returns per page
 p "You must set up config.yml - look at the example config.yml.dist" and exit unless File.exists?('config.yml')
 CONFIG = YAML.load_file('config.yml')
 
+# Crack, HTTParty's default json parser, doesn't handle Posterous' 
+# unclean json as well as the new core lib class
+module HTTParty
+  class JsonParser < HTTParty::Parser
+    SupportedFormats.merge!({"application/json" => :json, "text/json" => :json})
+    def json
+      JSON.parse(body)
+    end
+  end
+end
+
 class Posterous
   include HTTParty
   base_uri 'posterous.com/api/2'
   basic_auth CONFIG['username'], CONFIG['password']
+  parser HTTParty::JsonParser
 end
 
 response = Posterous.get('/auth/token')
@@ -46,10 +59,44 @@ total_pages = (post_count / PER_PAGE).ceil
     rescue StandardError => e
       date = ''
     end
-    slug = post['slug']
+    
+    image_urls = {}
+    post["media"].each do |media|
+      if(media["images"] && !media["images"].nil?)
+        media["images"].each do |image_and_sizes|
+          image_and_sizes.each do |size, image|
+            filename = image["url"].split('/').last
+            image_urls[image["url"]] = "/images/#{filename}"
+            image_response = HTTParty.get(image["url"])
+            next unless image_response.code == 200
+            img = File.new("images/#{filename}", "w")
+            img.puts image_response.parsed_response
+            img.close
+          end
+        end
+      end
+      
+      if(media["audio_files"] && !media["audio_files"].nil?)
+        media["audio_files"].each do |audio_files|
+          # TODO: build this
+        end
+      end
+      
+      if(media["videos"] && !media["videos"].nil?)
+        media["videos"].each do |video|
+          # TODO: build this
+        end
+      end
+    end
+    
+    slug = post["slug"]
     title = post["title"]
-    file = File.new("#{date}-#{slug}.html", "w")
-    file.puts post['body_full']
+    file = File.new("_posts/#{date}-#{slug}.md", "w")
+    body_full = post["body_full"]
+    image_urls.each {|old_url, new_url| body_full.gsub!(/#{old_url}/, new_url) }
+    file.puts body_full
+    file.close
+    
   end
   @pbar.inc
 end
